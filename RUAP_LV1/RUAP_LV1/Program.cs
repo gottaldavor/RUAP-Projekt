1f8e35351452673b4451d2cd077ef4773704e55a
Dario Lucić <lucicdario1998@gmail.com>
2023-11-07 08:39:07 +0100

    Add new project

---
+++ RUAP_LV1/RUAP_LV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP_LV1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
